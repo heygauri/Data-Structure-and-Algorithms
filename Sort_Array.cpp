eb184ad881431183d76561830a20c2268d8b41b7
heygauri <64316529+heygauri@users.noreply.github.com>
2023-09-28 18:26:42 +0530

    Create Sort_Array.cpp
    Add file for sort_array

---
+++ Sort_Array.cpp
@@ -0,0 +1,22 @@
+vector<int> sortArrayByParity(vector<int>& nums) {
+
+        // int n=size(nums);                            tc = O(n^2); sc = O(1)
+        // for(int i=0; i<n; i++) {
+        //     if(nums[i]%2 != 0) {
+        //         nums.push_back(nums[i]);
+        //         nums.erase(nums.begin()+i);
+        //         n--; i--;
+        //     }
+        // }
+        // return nums;
+
+        vector<int> ans;                                //tc = O(n); sc = O(n)
+        for(int &i:nums) {
+            if(i%2 == 0) ans.push_back(i);
+        }
+        for(int &i:nums) {
+            if(i%2 != 0) ans.push_back(i);
+        }   
+        return ans;
+
+    }
