d127547cbdb57d15c4e7db4d625144b637224dda
heygauri <64316529+heygauri@users.noreply.github.com>
2023-09-28 18:30:43 +0530

    Update Sort_Array.cpp
    Update

--- Sort_Array.cpp
+++ Sort_Array.cpp
@@ -10,13 +10,21 @@ vector<int> sortArrayByParity(vector<int>& nums) {
         // }
         // return nums;
 
-        vector<int> ans;                                //tc = O(n); sc = O(n)
-        for(int &i:nums) {
-            if(i%2 == 0) ans.push_back(i);
+        // vector<int> ans;                                //tc = O(n); sc = O(n)
+        // for(int &i:nums) {
+        //     if(i%2 == 0) ans.push_back(i);
+        // }
+        // for(int &i:nums) {
+        //     if(i%2 != 0) ans.push_back(i);
+        // }   
+        // return ans;
+
+        or(int i=0, j=0; i<size(nums) && j<size(nums); i++) {      tc = O(n); sc =O(1)
+            if(nums[i]%2 == 0) {
+                swap(nums[i], nums[j]);
+                j++;
+            }
         }
-        for(int &i:nums) {
-            if(i%2 != 0) ans.push_back(i);
-        }   
-        return ans;
+        return nums;
 
     }
