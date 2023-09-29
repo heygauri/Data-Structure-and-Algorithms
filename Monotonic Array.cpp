4b6799c4eebab98264f206df4babf60928434310
heygauri <64316529+heygauri@users.noreply.github.com>
2023-09-29 12:23:57 +0530

    Create Monotonic Array.cpp
    Add file Monotonic array

---
+++ Monotonic Array.cpp
@@ -0,0 +1,21 @@
+Link: https://leetcode.com/problems/monotonic-array/description/?envType=daily-question&envId=2023-09-29
+
+bool isMonotonic(vector<int>& nums) {
+        bool x{true}, y{true};                  //tc = O(n); sc = O(1)
+        //Asc order
+        for(int i=1; i<size(nums); i++) {
+            if(nums[i-1] > nums[i]) {
+                x = false;
+                break;
+            }
+        }
+        //Desc order
+        for(int i=1; i<size(nums); i++) {
+            if(nums[i-1] < nums[i]) {
+                y = false;
+                break;
+            }
+        }
+
+        return x || y;
+    }
